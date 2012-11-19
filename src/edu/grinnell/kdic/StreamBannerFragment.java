900de4a6f82fe15b99058e08beaed0ee7cc29ba2
Spencer B Liberto <libertos@grinnell.edu>
2012-11-18 23:55:51 -0600

    SVGs kiiiind of work
    Using the library svg-android, I was able to display SVGs as an
    imagebutton. Some SVGs do, such as the loading image. Some don't, such
    as the play and pause images. This is wierd, and I can't figure out why
    they get different treatment. However, it should be noted that play and
    pause images are about 100x bigger in size than the loading image.

--- src/edu/grinnell/kdic/StreamBannerFragment.java
+++ src/edu/grinnell/kdic/StreamBannerFragment.java
@@ -1,8 +1,14 @@
 package edu.grinnell.kdic;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import com.larvalabs.svgandroid.SVG;
+import com.larvalabs.svgandroid.SVGParser;
 
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
@@ -11,16 +17,27 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class StreamBannerFragment extends Fragment {
 
 	private String STREAMURL = "http://kdic.grinnell.edu:8001/kdic128";
+	private String IMAGEURL = "http://kdic.grinnell.edu/wp-content/uploads/EDM-150x150.gif";
 	
 	private MediaPlayer kdicStream = new MediaPlayer(); //KDIC stream
-    private Button playButton; //playPause button
-    //private ImageView metadataImage; //Metadata image. Duhh.
+    private ImageButton playButton; //playPause button
+    private ImageView metadataImage; //Metadata image. Duhh.
+    private TextView metadataText; //Double duhh.
+    private final ImageDownloader mDownload = new ImageDownloader();
     
     boolean isLoading = false; //true if stream is loading but not playing
+    
+    private SVG onPreparedButtonImage;
+    private SVG onStartButtonImage;
+    private SVG onStopButtonImage;
+    
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,48 +46,60 @@ public class StreamBannerFragment extends Fragment {
         
         
     }
-    
-    @Override
+
+	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-    	
     	
     	return inflater.inflate(R.layout.fragment_stream_banner, container, false);
     }
     
     @Override
     public void onViewCreated(View view, Bundle ofJoy){
-    	//Initial playPause settings
-        playButton = (Button) view.findViewById(R.id.playButton);
-        playButton.setText("Pause");
-        playButton.setBackgroundColor(Color.RED);
+    	
+    	//Initializing widget variables. 
+        playButton = (ImageButton) view.findViewById(R.id.playButton);
+        metadataImage = (ImageView) view.findViewById(R.id.curPlayingImage);
+        metadataText = (TextView) view.findViewById(R.id.curPlayingText);
         
-        //playPause listener, stops/starts stream
+        //initializing playButton images variables.
+        onStartButtonImage = SVGParser.getSVGFromResource(getResources(), R.drawable.loadingfinalgray); 
+        onPreparedButtonImage = SVGParser.getSVGFromResource(getResources(), R.drawable.pausefinal); 
+        onStopButtonImage = SVGParser.getSVGFromResource(getResources(), R.drawable.playfinal); 
+        
+        //playPause listener. Stops/starts stream
         playButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 playPause();
             }
         });
         
-        //stream prepared listener, changes button state to 'playing'
-        kdicStream.setOnPreparedListener(new android.media.MediaPlayer.OnPreparedListener() {
-        	public void onPrepared(MediaPlayer mp) {
-        		isLoading = false;
-                playButton.setText("Pause");
-                playButton.setBackgroundColor(Color.RED);
-        	}
-		});
+        // onPrepared listener. Starts stream and changes playButton image when the stream has finished setting up.
+        kdicStream.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+			@Override
+			public void onPrepared(MediaPlayer mp) {
+			       kdicStream.start();
+			       isLoading = false;
+			       playButton.setImageDrawable(onPreparedButtonImage.createPictureDrawable());
+			}
+        });
+
+        // Set metadata image to hardcoded URL
+        mDownload.download(IMAGEURL, metadataImage);
         
         //Starts Stream
         setupPlayer();
         if (!(kdicStream.isPlaying())){
         	startPlaying();
         }
+        
     }
     
   //If the stream is not stopped, stop. Else, start.
     public void playPause(){
-    		if( (isLoading) || (kdicStream.isPlaying()) ){
+    		if(isLoading){
+    			// do nothing
+    		} else if ((kdicStream.isPlaying())) {
     			stopPlaying();
     		} else {
     			startPlaying();
@@ -80,25 +109,19 @@ public class StreamBannerFragment extends Fragment {
     //Changes playPause to 'loading' state, prepares stream, starts stream
     public void startPlaying(){
     	isLoading = true;
-    	playButton.setText("Starting..."); // WHY WON'T THIS APPEAR FOR MORE THAN A FRACTION OF A SECOND.
-        playButton.setBackgroundColor(Color.YELLOW);
+	    playButton.setImageDrawable(onStartButtonImage.createPictureDrawable());
     	
     	try {
-    		kdicStream.prepare();
+    		kdicStream.prepareAsync();
 		} catch (IllegalStateException e) {
 			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} // might take long! (for buffering, etc)
-        
-       kdicStream.start();
+		}
     }
     
     //Stops stream, changes playPause to 'stopped' state.
     public void stopPlaying(){
     	kdicStream.stop();
-    	playButton.setText("Play");
-        playButton.setBackgroundColor(Color.GREEN);
+	       playButton.setImageDrawable(onStopButtonImage.createPictureDrawable());
     }
     
     //Sets stream's type and URL
@@ -115,6 +138,7 @@ public class StreamBannerFragment extends Fragment {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-    }
         
+    }
+    
 }
